a6a5df9cb44c7d52f54e33a2df36657294ebbde1
Tam Pham <tampham47@live.com>
2015-09-30 11:27:00 +0700

    count-to-6 2 functions

---
+++ 01-hello-es6.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// class CountToSix {
+// 	constructor() {
+// 	}
+
+// 	helloES6() {
+// 		console.log('Hello ES6');
+// 	}
+// }
+
+// var countToSix = new CountToSix();
+// countToSix.helloES6();
+
+console.log('HELLO ES6');
---
+++ 02-template-string.js
@@ -0,0 +1,4 @@
+var userName = process.argv[2];
+
+console.log(`Hello, ${userName}!`);
+console.log(`Your name lowercased is "${userName.toLowerCase()}".`);
